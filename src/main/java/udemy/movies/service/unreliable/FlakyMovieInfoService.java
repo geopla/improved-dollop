f68596d435d9d69aff8a87a83318412a8355e27c
Georg Plafinger <georg.plafinger@datev.de>
2025-07-18 18:04:45 +0200

    investigate onErrorReturn()

--- src/main/java/udemy/movies/service/unreliable/FlakyMovieInfoService.java
+++ src/main/java/udemy/movies/service/unreliable/FlakyMovieInfoService.java
@@ -1,10 +1,19 @@
 package udemy.movies.service.unreliable;
 
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import udemy.movies.domain.MovieInfo;
 import udemy.movies.service.MovieInfoService;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class FlakyMovieInfoService {
+
+    final static List<MovieInfo> fallbackMovieInfos = List.of(
+            new MovieInfo(42L, "The Big Lebowski", List.of("John Goodman", "Jeff Bridges", "Steve Buscemi"), 1998, LocalDate.of(1998, 3, 6)),
+            new MovieInfo(102, "Pulp Fiction", List.of("John Travolta", "Samuel L. Jackson", "Bruce Willis"), 1994, LocalDate.of(1994, 11, 03))
+    );
 
     private final MovieInfoService movieInfoService;
 
@@ -16,5 +25,10 @@ public class FlakyMovieInfoService {
         return movieInfoService.movieInfos()
                 .take(1)
                 .concatWith(Flux.error(new IllegalStateException("oopsie")));
+    }
+
+    public Flux<MovieInfo> movieInfosOnErrorReturn() {
+        return movieInfos()
+                .onErrorReturn(fallbackMovieInfos.getFirst());
     }
 }
--- src/test/java/udemy/movies/service/unreliable/FlakyMovieInfoServiceTest.java
+++ src/test/java/udemy/movies/service/unreliable/FlakyMovieInfoServiceTest.java
@@ -3,7 +3,13 @@ package udemy.movies.service.unreliable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
+import udemy.movies.domain.MovieInfo;
 import udemy.movies.service.MovieInfoService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
 
 class FlakyMovieInfoServiceTest {
 
@@ -18,5 +24,24 @@ class FlakyMovieInfoServiceTest {
                 .expectNextMatches(movieInfo -> movieInfo.name().equals("Batman Begins"))
                 .expectErrorMatches(e -> e.getMessage().contains("oopsie"))
                 .verify();
+    }
+
+    @Test
+    @DisplayName("Should use a fallback movie info in case of an error")
+    void shouldUseFallbackMovieInfo() {
+        List<MovieInfo> movieInfos = new ArrayList<>();
+
+        StepVerifier.create(flakyMovieInfoService.movieInfosOnErrorReturn())
+                .recordWith(() -> movieInfos)
+                .expectNextCount(2)
+                .expectRecordedMatches(containsAllMovieNames("Batman Begins","The Big Lebowski"))
+                .verifyComplete();
+    }
+
+    private static Predicate<Collection<MovieInfo>> containsAllMovieNames(String ... names) {
+        return movieInfo -> movieInfo.stream()
+                .map(MovieInfo::name)
+                .toList()
+                .containsAll(List.of(names));
     }
 }
